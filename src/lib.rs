c74ac85b22ed3957638c9bfb2c96aad864d86939
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-04-20 16:28:27 -0400

    Define same latitude as ignoring the degrees to get more candidates
    Might want to tinker with this and do plus or minus a number of minutes
    instead, but this works for now.

--- src/lib.rs
+++ src/lib.rs
@@ -70,7 +70,7 @@ fn same_latitude(lat: Coord) -> Vec<City> {
         .iter()
         .cloned()
         .filter(|city| {
-            city.latitude == lat
+            city.latitude.deg == lat.deg && city.latitude.dir == lat.dir
         })
         .collect()
 }
@@ -81,13 +81,13 @@ mod tests {
 
     #[test]
     fn it_finds_cities_with_same_latitude() {
-        let lat = Coord { deg: 40, min: 25, dir: 'N'};
+        let lat = Coord { deg: 40, min: 25, dir: 'N' };
         let cities = same_latitude(lat);
         let mut names: Vec<_> = cities.iter().map(|ref c| &c.name).collect();
         names.sort();
         assert_eq!(
             names,
-            vec!["Greeley", "Lafayette", "Pittsburgh"]
+            vec!["Adapazari", "Agdam", "Alexandroupoli", "Allentown", "Altoona", "Amasya", "Andijon", "Anxi", "Aomori", "Arcata", "Ashtarak", "Aveiro", "Baku", "Baotou", "Beaver Falls", "Berat", "Bilecik", "Bloomington", "Bolu", "Boulder", "Brindisi", "Burlington", "Bursa", "Canakkale", "Cankiri", "Canton", "Changping", "Chengde", "Chosan", "Coimbra", "Corovode", "Corum", "Covilha", "Craig", "Dandong", "Datong", "Dunhuang", "Elko", "Erseke", "Eureka", "Fargona", "Fengzhen", "Fier", "Fort Collins", "Gadabay", "Galesburg", "Ganca", "Gavarr", "Giresun", "Gjirokaster", "Goranboy", "Goycay", "Gramsh", "Grand Island", "Greeley", "Guadalajara", "Guarda", "Guliston", "Gumushane", "Gyumri", "Hachinohe", "Hanggin Houqi", "Harrisburg", "Hirosaki", "Hohhot", "Ijevan", "Izmit", "Jalal Abad", "Jinxi", "Jizzax", "Johnstown", "Kanggye", "Kars", "Katerini", "Kavala", "Kearney", "Khujand", "Kilchu", "Kimchaek", "Kimhyonggwon", "Kirksville", "Kokomo", "Konibodom", "Korce", "Lafayette", "Lancaster", "Lecce", "Lima", "Lincoln", "Lushnje", "Madrid", "Mansfield", "Marion", "McCook", "Muncie", "Naples", "Navoi", "New York", "Newark", "Olbia", "Olmaliq", "Osh", "Paterson", "Peoria", "Permet", "Pittsburgh", "Pogradec", "Polygyros", "Potenza", "Provo", "Qabala", "Qoqon", "Redding", "Sakarya", "Salamanca", "Salerno", "Salt Lake City", "Sassari", "Sinuiju", "State College", "Sumqayt", "Taedong", "Taranto", "Tekirdag", "Tepelene", "Thessaloniki", "Tokat", "Tovuz", "Trabzon", "Trenton", "Turkmenbasy", "Urbana", "Vanadzor", "Vernal", "Viseu", "Vlore", "Wheeling", "Winnemucca", "Xuanhua", "Yerevan", "Yevlax", "Yingkow", "Zhangjiakou"]
         );
     }
 }
